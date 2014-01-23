74cdf8cc6c66b87ce3e946d6d56a8b52fa679713
Neeraj Sharma <neeraj.sharma@alumni.iitg.ernet.in>
2014-01-23 09:50:31 +0530

    Add a sample app.

---
+++ sampleapp.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python -u
+"""
+A sample test application which uses the txtscrump.
+
+copyright (c) 2013 Neeraj Sharma <neeraj.sharma@alumni.iitg.ernet.in>
+License: See LICENSE file
+"""
+
+
+import logging
+
+logging.basicConfig(format='%(message)s', level=logging.INFO)
+LOG = logging.getLogger(os.path.basename(__name__))
+""" The logging instance to log messages for this module."""
+
